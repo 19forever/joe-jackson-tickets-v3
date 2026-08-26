14d79a3fb632ce6385d681c2a4b9c038628613f0
19forever <marek.kraus@gmail.com>
2026-08-26 09:37:24 +0200

    Initialize Supabase client in supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,6 @@
+// supabase-client.js
+const SUPABASE_URL = 'https://eaefcjaqkbgkxnvqmqkg.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_1HFppA7vWugNKQfk0j6CkA_PC7jjaOA';
+
+// Inicializace Supabase klienta
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
