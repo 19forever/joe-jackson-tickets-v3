a7ca53f47c29bea580e9a68e3859f087afad9260
19forever <marek.kraus@gmail.com>
2026-09-08 20:31:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1300,7 +1300,7 @@ function renderCategoryTabs(matchesBeforeCategoryFilter) {
 
   const categoryOrder = isAdmin 
     ? ['Tickets', 'Passes', 'Programs', 'Posters', 'T-shirts', 'Tour Items', 'Memorabilia', 'Videos', 'ALL']
-    : ['Tickets', 'Passes', 'ALL'];
+    : ['Tickets', 'Passes'];
 
   const categoryLabels = { 
     'Tickets': '🎫 Tickets', 'Passes': '🪪 Passes', 'Programs': '📖 Programs', 
